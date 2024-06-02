70eca9dee9d20492d15f71658e2986b5d9e5c7a2
Terry Babyuk <tbabyuk@gmail.com>
2024-06-02 11:41:27 -0400

    added weather widget to navigation

--- js/script.js
+++ js/script.js
@@ -37,4 +37,44 @@ carouselButton.addEventListener('click', function () {
         faIcon.classList.add('fa-pause');
         carousel.cycle();
     }
-})   
+})
+
+
+
+const fetchWeather = async () => {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "toronto";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayWeather(data);
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+fetchWeather();
+
+
+const displayWeather = ({main, weather}) => {
+    console.log("showing main:", main);
+    console.log("showing weather:", weather)
+    const temperature = main.temp;
+    const description = weather[0].description;
+    const icon = weather[0].icon;
+
+    // add weather image
+    const weatherIcon = document.querySelector("#weather-icon");
+    weatherIcon.src = `https://openweathermap.org/img/w/${icon}.png`
+
+    // add temperature
+    const tempDisplay = document.querySelector("#weather-temp");
+    tempDisplay.innerText = temperature + "\u00B0";
+
+    // add description
+    const descriptionDisplay = document.querySelector("#weather-description");
+    descriptionDisplay.innerText = description;
+    console.log("logging all data:", temperature, description, icon)
+}
